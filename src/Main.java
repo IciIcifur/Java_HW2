44daadf20d159001baac5ab13bb2cd9c4ce8b9ee
IciIcifur <iciicifur@yandex.ru>
2024-03-15 16:40:04 +0300

    Improved strategy for results' storage.

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("""
                 \nThis program will count all latin and cyrillic symbols met in a file.
-                \u001B[36mYou do not need to specify result file's extension, results will be found in /results/.\u001B[0m\s""");
+                \u001B[36mYou do not need to specify result file's extension.\u001B[0m\s""");
 
         Scanner in = new Scanner(System.in);
+
+        String resultsPath = "";
         try {
-            Path resultsDirectory = Path.of("results");
-            if (!Files.exists(resultsDirectory)) Files.createDirectory(resultsDirectory);
-        } catch (IOException e) {
-            System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
+            // SELECTING A DIRECTORY FOR RESULTS
+            System.out.println("\n\nWhere do you want to store results?");
+            String resultsDirectory = in.nextLine();
+            resultsPath = resultsDirectory + "/results";
+
+            if (Files.exists(Path.of(resultsDirectory)) && !Files.exists(Path.of(resultsPath))) {
+                Files.createDirectory(Path.of(resultsPath));
+            } else
+                throw new RuntimeException("Directory doesn't exist or already contains directory 'results'.");
+        } catch (IOException | RuntimeException e) {
+            System.out.println("\n\u001B[31m" + e.getMessage() + "\u001B[0m");
+            System.exit(1);
         }
 
         while (true) {
@@ -25,12 +35,12 @@ public class Main {
                 FileInfo.checkExistence(filePath);
 
                 System.out.print("\nEnter filename for results: ");
-                Path resultsPath = Path.of("results\\" + in.nextLine() + ".txt");
-                if (Files.exists(resultsPath)) throw new RuntimeException(resultsPath + " already exists!");
-                Files.createFile(resultsPath);
+                Path resultFile = Path.of(resultsPath + "/" + in.nextLine() + ".txt");
+                if (Files.exists(resultFile)) throw new RuntimeException(resultFile + " already exists!");
+                Files.createFile(resultFile);
 
                 // PERFORM CHECKS
-                FileInfo.checkFileInfo(filePath, resultsPath);
+                FileInfo.checkFileInfo(filePath, resultFile);
 
                 // EXIT
                 System.out.print("\nDo you want to exit? (1/0) ");
@@ -38,7 +48,7 @@ public class Main {
                 if (flag == 1) break;
                 else in.nextLine();
             } catch (RuntimeException | IOException e) {
-                System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
+                System.out.println("\n\u001B[31m" + e.getMessage() + "\u001B[0m");
             }
         }
     }
